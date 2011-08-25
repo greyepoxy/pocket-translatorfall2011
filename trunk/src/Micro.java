08a5d99f11a74ed7e8378502b6cf68d3044f5111
Bowman.Brian.A@gmail.com <Bowman.Brian.A@gmail.com@6a350a6b-1625-0d96-7f13-1d115c92b19d>
2011-08-25 18:23:56 +0000

    Test Commit

--- trunk/src/Micro.java
+++ trunk/src/Micro.java
@@ -5,7 +5,7 @@ public class Micro {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("HelloWorld I will be a compiler eventually");
+		System.out.println("HelloWorld I will be a compiler eventually!!!!!!");
 
 	}
 
