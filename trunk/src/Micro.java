b294e4e79080c5a940316cbde2d4debe53cb31aa
bowman.brian.a <bowman.brian.a@6a350a6b-1625-0d96-7f13-1d115c92b19d>
2011-08-27 23:28:38 +0000

    Added the ANTLR grammar file and generated lexer.  Edited Micro.java to instantiate a lexer on a file (passed on the command line) and spit out types and values.  We may want to look at a cleaner way of doing the types.

--- trunk/src/Micro.java
+++ trunk/src/Micro.java
@@ -1,3 +1,10 @@
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.antlr.runtime.ANTLRFileStream;
+import org.antlr.runtime.CharStream;
+import org.antlr.runtime.CommonTokenStream;
+import org.antlr.runtime.Token;
 
 public class Micro {
 
@@ -5,8 +12,59 @@ public class Micro {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("HelloWorld I will be a compiler eventually!!!!!!");
-
+		
+		//lets do some file io		
+		try
+		{
+			CharStream inputstream = new ANTLRFileStream(args[0].trim());			
+			MicroLexer lexer = new MicroLexer(inputstream);
+			Token t = lexer.nextToken();
+			while (t.getType() != -1)
+			{
+				if((t.getType()) != lexer.WS && (t.getType() != lexer.COMMENT))
+				{
+					String type = "";
+					switch(t.getType())
+					{
+					case MicroLexer.FLOATLITERAL:
+						type = "FLOATLITERAL";
+						break;
+					case MicroLexer.IDENTIFIER:
+						type = "IDENTIFER";
+						break;
+					case MicroLexer.INTLITERAL:
+						type = "INTLITERAL";
+						break;
+					case MicroLexer.KEYWORD:
+						type = "KEYWORD";
+						break;
+					case MicroLexer.OPERATOR:
+						type = "OPERATOR";
+						break;
+					case MicroLexer.STRINGLITERAL:
+						type = "STRINGLITERAL";
+						break;
+					default:
+						System.out.println("Invalid Token type");
+						System.exit(1);
+							
+					}
+					
+					System.out.println("Type: " + type);
+					System.out.println("Value: " + t.getText());					
+				}
+				
+				t = lexer.nextToken();
+			}
+			
+		}
+		catch(IOException e)
+		{
+			System.out.println("Couldn't find the file");
+		}
+		
+		
 	}
 
 }
+
---
+++ trunk/src/MicroParser.g
@@ -0,0 +1,37 @@
+lexer grammar MicroParser;
+
+KEYWORD 
+   :  ('PROGRAM' | 'BEGIN' | 'END' | 'PROTO' | 'FUNCTION' | 'READ' | 'WRITE' | 
+   'IF' | 'THEN' | 'ELSE' | 'ENDIF' | 'RETURN' | 'CASE' | 'ENDCASE' | 'BREAK' | 
+   'DEFAULT' | 'DO' | 'WHILE' | 'FLOAT' | 'INT' | 'VOID' | 'STRING')	
+   ; 
+
+IDENTIFIER  :	('a'..'z'|'A'..'Z'|'_') ('a'..'z'|'A'..'Z'|'0'..'9'|'_')*
+    ;
+
+INTLITERAL :	'0'..'9'+
+    ;
+
+FLOATLITERAL
+    :   ('0'..'9')+ '.' ('0'..'9')*
+    |   '.' ('0'..'9')+
+    ;
+
+COMMENT
+    :   '--' ~('\n'|'\r')* '\r'? '\n' {$channel=HIDDEN;}
+    ;
+
+OPERATOR
+   :  (':='|'+'|'-'|'*'|'/'|'='|'<'|'>'|'('|')'|';'|','|'!')
+   ;
+
+STRINGLITERAL
+    :  '"' ( ~('"') )* '"'
+    ;
+    
+WS  :   ( ' '
+        | '\t'
+        | '\r'
+        | '\n'
+        ) {$channel=HIDDEN;}
+    ;
